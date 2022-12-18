d3914f30c79e733742cad73fc9b96d670343a3ef
Krzysiek <krzysiek.pienkowski@gmail.com>
2022-12-18 23:06:56 +0100

    Initial commit

---
+++ src/main/java/pl/convista/demoScheduler/DemoSchedulerApplication.java
@@ -0,0 +1,13 @@
+package pl.convista.demoScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/convista/demoScheduler/DemoSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.convista.demoScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
